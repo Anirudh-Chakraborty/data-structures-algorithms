50ed0c6dd6a24ca7e990992700b08aba6940eeef
anirudhchakraborty <preetiujjal@gmail.com>
2026-09-22 22:05:58 +0530

    Lc 2235 done

---
+++ Lc2235.java
@@ -0,0 +1,9 @@
+public class Lc2235 {
+    public static void main(String[] args) {
+        Lc2235 caller = new Lc2235();
+        caller.sum(4, 5);
+    }
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
